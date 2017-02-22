66f5e426d945f11d83a69b01bb21d2baeff1e4d5
yonixw <yonixw@gmail.com>
2017-02-22 21:58:55 +0200

    refactoring the 1 function i have

--- app/src/main/java/us/cpluspl/yonixw/readheartbatmiband2/MainActivity.java
+++ app/src/main/java/us/cpluspl/yonixw/readheartbatmiband2/MainActivity.java
@@ -6,21 +6,7 @@ import android.view.View;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothGatt;
-import android.bluetooth.BluetoothGattCallback;
-import android.bluetooth.BluetoothGattCharacteristic;
-import android.bluetooth.BluetoothGattService;
-import android.bluetooth.BluetoothProfile;
-import android.content.Context;
-import android.os.Handler;
-import android.os.Looper;
 import android.util.Log;
-
-import java.util.Arrays;
-import java.util.Set;
-import java.util.UUID;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = "YONI-MI-2";
@@ -35,23 +21,33 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void btnRun(View view) {
-        logAllBluetoothDevices();
+        // Like network card, connect to all devices in Bluetooth (like PC in Netowrk)
+        final BluetoothAdapter myBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        findMiBandBluetooth(myBluetoothAdapter);
     }
 
-    void logAllBluetoothDevices() {
+    BluetoothDevice findMiBandBluetooth(BluetoothAdapter myBluetoothAdapter) {
         Log.d(TAG, "Initialising Bluetooth connection");
 
-        if(BluetoothAdapter.getDefaultAdapter().isEnabled()) {
-            final BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-            final Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
-
-            for (BluetoothDevice pairedDevice : pairedDevices) {
+        if(myBluetoothAdapter.isEnabled()) {
+            for (BluetoothDevice pairedDevice : myBluetoothAdapter.getBondedDevices()) {
                 if (pairedDevice.getName().contains("MI")) {
                     Log.d(TAG, "Name: " +  pairedDevice.getName());
                     Log.d(TAG, "MAC: " + pairedDevice.getAddress());
+
+                    return pairedDevice;
                 }
             }
         }
+
+        return  null; // Not found
     }
 
 }
+
+/*
+Credit and thanks:
+
+https://github.com/lwis/miband-notifier/
+
+*/
