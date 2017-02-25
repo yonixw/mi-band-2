f2fc7ab18555978cadc7ffea51a539bf72a413ea
yonixw <yonixw@gmail.com>
2017-02-26 01:47:35 +0200

    Now getting heart rate. You could start it. Need to use descriptor. Also getting notification when other initiate heartbeat scan.
    02-26 01:43:22.804 5951-5951/us.cpluspl.yonixw.readheartbatmiband2 D/YONI-MI-2: * Getting gatt servie for heartbeat
    02-26 01:43:22.804 5951-5951/us.cpluspl.yonixw.readheartbatmiband2 D/YONI-MI-2: * Getting gatt Characteristic for hearbeat trigger
    02-26 01:43:22.804 5951-5951/us.cpluspl.yonixw.readheartbatmiband2 D/YONI-MI-2: * Writing trigger
    02-26 01:43:22.810 5951-5951/us.cpluspl.yonixw.readheartbatmiband2 D/YONI-MI-2: * Writting trigger status :true
    02-26 01:43:23.481 5951-5967/us.cpluspl.yonixw.readheartbatmiband2 D/YONI-MI-2: Write successful: [21, 2, 1]
    02-26 01:43:30.727 5951-5968/us.cpluspl.yonixw.readheartbatmiband2 D/YONI-MI-2:  - Notifiaction UUID: 00002a37-0000-1000-8000-00805f9b34fb
    02-26 01:43:30.728 5951-5968/us.cpluspl.yonixw.readheartbatmiband2 D/YONI-MI-2:  - Notifiaction value: [0, 76]

--- app/src/main/java/us/cpluspl/yonixw/readheartbatmiband2/Consts.java
+++ app/src/main/java/us/cpluspl/yonixw/readheartbatmiband2/Consts.java
@@ -13,12 +13,20 @@ public class Consts {
     public static final UUID UUID_SERVICE_GENERIC = UUID.fromString(String.format(BASE_UUID, "1800"));
     public static final UUID UUID_SERVICE_MIBAND_SERVICE = UUID.fromString(String.format(BASE_UUID, "FEE0"));
     public static final UUID UUID_SERVICE_MIBAND2_SERVICE = UUID.fromString(String.format(BASE_UUID, "FEE1"));
+    public static final UUID UUID_SERVICE_HEARTBEAT = UUID.fromString(String.format(BASE_UUID, "180D"));
 
     // General service
     public static final UUID UUID_CHARACTERISTIC_DEVICE_NAME = UUID.fromString(String.format(BASE_UUID, "2A00"));
 
     // Miband service 1
     public static final UUID UUID_BUTTON_TOUCH = UUID.fromString("00000010-0000-3512-2118-0009af100700");
+
+    //Heart beat service
+    public static final UUID UUID_START_HEARTRATE_CONTROL_POINT = UUID.fromString("00002a39-0000-1000-8000-00805f9b34fb");
+    public static final UUID UUID_NOTIFICATION_HEARTRATE = UUID.fromString("00002a37-0000-1000-8000-00805f9b34fb");
+    public static final byte[] BYTE_LAST_HEART_RATE_SCAN = {21, 1, 1};
+    public static final byte[] BYTE_NEW_HEART_RATE_SCAN = {21, 2, 1};
+    public static final UUID UUID_DESCRIPTOR_UPDATE_NOTIFICATION = UUID.fromString("00002902-0000-1000-8000-00805f9b34fb");
 
     final protected static char[] hexArray = "0123456789ABCDEF".toCharArray();
     public static String bytesToHex(byte[] bytes) {
--- app/src/main/java/us/cpluspl/yonixw/readheartbatmiband2/MainActivity.java
+++ app/src/main/java/us/cpluspl/yonixw/readheartbatmiband2/MainActivity.java
@@ -3,6 +3,7 @@ package us.cpluspl.yonixw.readheartbatmiband2;
 import android.bluetooth.BluetoothGatt;
 import android.bluetooth.BluetoothGattCallback;
 import android.bluetooth.BluetoothGattCharacteristic;
+import android.bluetooth.BluetoothGattDescriptor;
 import android.bluetooth.BluetoothGattService;
 import android.bluetooth.BluetoothProfile;
 import android.content.Context;
@@ -17,7 +18,6 @@ import android.bluetooth.BluetoothDevice;
 import android.util.Log;
 
 import java.util.Arrays;
-import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
     public static final String TAG = "YONI-MI-2";
@@ -98,6 +98,7 @@ public class MainActivity extends AppCompatActivity {
         public void onCharacteristicWrite(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status)
         {
             Log.d(TAG, "Write successful: " + Arrays.toString(characteristic.getValue()));
+            super.onCharacteristicWrite(gatt,characteristic,status);
         }
 
         @Override
@@ -108,9 +109,12 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public void onCharacteristicChanged(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic) {
+            Log.d(TAG, " - Notifiaction UUID: " +  characteristic.getUuid().toString());
+            Log.d(TAG, " - Notifiaction value: " +  Arrays.toString(characteristic.getValue()));
             super.onCharacteristicChanged(gatt, characteristic);
-            Log.d(TAG, "Got notification: " +  Arrays.toString(characteristic.getValue()));
         }
+
+
     };
 
     BluetoothGatt myGatBand;
@@ -187,10 +191,90 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void btnTest(View view) {
+    public void getHeartBeat() throws InterruptedException {
+        /*
+        Steps to read heartbeat:
+            - Register Notification (like in touch press)
+                - Extra step with description
+            - Write predefined bytes to control_point to trigger measurement
+            - Listener will get result
+        */
+
+        Log.d(TAG, "* Getting gatt servie for heartbeat");
+        BluetoothGattService myGatService =
+                myGatBand.getService(Consts.UUID_SERVICE_HEARTBEAT);
+        if (myGatService != null) {
+            Log.d(TAG, "* Getting gatt Characteristic for hearbeat callback");
+
+            for (BluetoothGattCharacteristic c: myGatService.getCharacteristics()) {
+                Log.d(TAG,"Found: " + c.getUuid().toString());
+            }
+
+            BluetoothGattCharacteristic myGatChar
+                    = myGatService.getCharacteristic(Consts.UUID_NOTIFICATION_HEARTRATE);
+            if (myGatChar != null) {
+
+                Log.d(TAG, "* Statring listening");
+
+                // second parametes is for starting\stopping the listener.
+                boolean status =  myGatBand.setCharacteristicNotification(myGatChar, true);
+                Log.d(TAG, "* Set notification status :" + status);
+
+                // Set Descriptor:
+                BluetoothGattDescriptor myDescriptor
+                    = myGatChar.getDescriptor(Consts.UUID_DESCRIPTOR_UPDATE_NOTIFICATION);
+                if (myDescriptor != null) {
+                    Log.d(TAG, "Writing decriptors");
+                    myDescriptor.setValue(BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE);
+                    status = myGatBand.writeDescriptor(myDescriptor);
+                    Log.d(TAG, "Writing decriptors result: " + status);
+                }
+
+            }
+
+            Thread.sleep(5000,0);
+
+            getNewHeartBeat();
+        }
+
+
+
+    }
+
+    public void getNewHeartBeat() throws InterruptedException {
+        Log.d(TAG, "* Getting gatt servie for heartbeat");
+        BluetoothGattService myGatService =
+                myGatBand.getService(Consts.UUID_SERVICE_HEARTBEAT);
+        if (myGatService != null) {
+            // Now write to trigger heartbeat:
+            Log.d(TAG, "* Getting gatt Characteristic for hearbeat trigger");
+            BluetoothGattCharacteristic myGatChar
+                    = myGatService.getCharacteristic(Consts.UUID_START_HEARTRATE_CONTROL_POINT);
+            if (myGatChar != null) {
+                Log.d(TAG, "* Writing trigger");
+                myGatChar.setValue(Consts.BYTE_NEW_HEART_RATE_SCAN);
+
+                boolean status =  myGatBand.writeCharacteristic(myGatChar);
+                Log.d(TAG, "* Writting trigger status :" + status);
+            }
+        }
+    }
+
+    public void btnTest(View view) throws InterruptedException {
         //readDataExampleToTest();
         getTouchNotifications();
+
+
     }
+
+    public void btnSetuphearRate(View view) throws InterruptedException {
+        getHeartBeat();
+    }
+
+    public void btnTestHeartRate(View view) throws InterruptedException {
+        getNewHeartBeat();
+    }
+
 }
 
 /*
@@ -204,5 +288,11 @@ http://stackoverflow.com/questions/20043388/working-with-ble-android-4-3-how-to-
 // Available services\Characteristics:
 http://jellygom.com/2016/09/30/Mi-Band-UUID.html
 https://github.com/Freeyourgadget/Gadgetbridge/blob/master/app/src/main/java/nodomain/freeyourgadget/gadgetbridge/devices/miband/MiBand2Service.java
+https://devzone.nordicsemi.com/question/310/notificationindication-difference/
 
+// Heartbeat info
+http://stackoverflow.com/questions/36311874/heart-rate-measuring-using-xiaomi-miband-and-ble
+https://github.com/AlexanderHryk/MiFood/
+again: http://stackoverflow.com/questions/20043388/working-with-ble-android-4-3-how-to-write-characteristics
+https://github.com/dkhmelenko/miband-android/blob/master/miband-sdk/src/main/java/com/khmelenko/lab/miband/model/Protocol.java
 */
